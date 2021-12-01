83688355fe9cd7e0747823cf0d882a0a18392f4c
machadoma <70416384+machadoma@users.noreply.github.com>
2021-12-01 18:35:27 -0300

    Add files via upload

---
+++ Client (2).py
@@ -0,0 +1,58 @@
+from socket import *
+import pickle
+import os 
+
+serverName = "127.0.0.1"
+serverPort = 27123
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+pkmon = ["Bulbasaur", "Charmander", "Squirtle"]
+escolha = 4
+md5 = 1
+vic = 0
+lose = 0
+
+
+
+print("Escolha seu pokémon 1 - Bulbasaur, 2 - Charmander ou 3 - Squirtle.\n")
+print("Bulbasaur > Squirtle > Charmander > Bulbasaur")
+
+while (md5 <= 5) or (vic <= 3) or (lose <= 3):
+    if (md5 > 5) or (vic >= 3) or (lose >= 3):
+        break
+    while escolha <=0 or escolha >=4:
+       
+        print(f"Rodada:{md5}")
+        print(f"Placar Vitorias: {vic} x {lose} :Derrotas")
+        escolha = int(input("Escolha seu pokémon: "))
+        
+
+    x = escolha.to_bytes((escolha.bit_length() + 7) // 8, byteorder='little')
+    clientSocket.send(bytes(x))
+    result = clientSocket.recv(1024)
+    escolha = 4
+    text = pickle.loads(result)
+    
+        
+    if (text == "Vitória"):
+        md5 += 1
+        vic += 1
+    if (text == "Derrota"):
+        md5 += 1
+        lose += 1 
+    
+
+  
+
+if (md5 > 5):
+     print('Jogo encerrado')
+        
+if (vic >= 3 ):
+     print('Você mostrou quem é o mestre pokémon')
+        
+if (lose >= 3 ):
+     print('Foi de base')
+
+os.system("pause")     
+clientSocket.close()
---
+++ Server (1).py
@@ -0,0 +1,67 @@
+from socket import *
+import pickle
+import os 
+
+serverPort = 27123
+serverSocket = socket(AF_INET, SOCK_STREAM)
+HOST = '127.0.0.1'
+serverSocket.bind(("", serverPort))
+serverSocket.listen(5)
+print("Aguardando outro jogador\n")
+connectionSocket, addr = serverSocket.accept()
+
+pkmon = ["Bulbasaur", "Charmander", "Squirtle"]
+escolha = 4
+md5 = 1
+vic = 0
+lose = 0
+
+
+print("Escolha seu pokémon 1 - Bulbasaur, 2 - Charmander ou 3 - Squirtle.\n")
+print("Bulbasaur > Squirtle > Charmander > Bulbasaur")
+
+while (md5 <= 5) or (vic <= 3) or (lose <= 3):
+    if (md5 > 5) or (vic >= 3) or (lose >= 3):
+        break
+    while escolha <=0 or escolha >=4:
+        print(f"Rodada: {md5}")
+        print(f"Placar Vitorias: {vic} x {lose} : Derrotas")
+        escolha = int(input("Escolha seu pokémon: "))
+        
+        
+
+    received = connectionSocket.recv(1024)
+    escolhaini = int.from_bytes(received, byteorder="little")
+
+    if(escolha == escolhaini):
+        response = "Empate"
+        print("Empate")
+       
+    if((escolha== 2 and escolhaini == 1) or (escolha== 1 and escolhaini == 3) or (escolha== 3 and escolhaini == 2)):
+        response = "Derrota"
+        print("Você Venceu")
+        md5 += 1
+        vic += 1
+      
+    if ((escolha == 1 and escolhaini == 2) or (escolha == 3 and escolhaini == 1) or (escolha == 2 and escolhaini == 3)):
+        response = "Vitória"
+        print("Você Perdeu")
+        lose += 1
+        md5 += 1
+        
+    connectionSocket.send(pickle.dumps(response))
+    escolha = 4
+ 
+
+
+if (md5 >= 5):
+        print('Jogo encerrado')
+   
+if (vic >= 3 ):
+        print('Você mostrou quem é o mestre pokémon')
+       
+if (lose >= 3 ):
+        print('Foi de base')
+      
+os.system("pause")
+connectionSocket.close()
